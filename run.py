cac1218602514e4fb12d566e10fa430c417a10e8
Edward Betts <edward@4angle.com>
2017-03-15 07:59:05 +0000

    change to port 5001
     - my laptop has another project running on port 5000

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from matcher.view import app
 if __name__ == "__main__":
     app.config.from_object('config.default')
     app.debug = True
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', port=5001)
