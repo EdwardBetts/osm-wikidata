fdde02505ad40c3a52da49f687121b397e06b704
Edward Betts <edward@4angle.com>
2017-03-18 07:26:26 +0000

    Initialise the database when running app.

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 from matcher.view import app
+from matcher import database
 
 if __name__ == "__main__":
     app.config.from_object('config.default')
+    database.init_app(app)
     app.debug = True
     app.run('0.0.0.0', port=5001)
