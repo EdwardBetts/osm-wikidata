3b02b7d42d4d1fc0f010fa1a97447c549436287c
Edward Betts <edward@4angle.com>
2017-05-27 14:43:34 +0100

    add e.preventDefault

--- matcher/static/js/app.js
+++ matcher/static/js/app.js
@@ -5,6 +5,7 @@ $(function() {
   $('.show_tags_link').click(function(e) {
     var link = $(this);
     var show_all = 'show all tags';
+    e.preventDefault();
     $('#candidate' + link.data('key')).toggle();
     link.text(link.text() == show_all ? 'hide tags' : show_all);
 
