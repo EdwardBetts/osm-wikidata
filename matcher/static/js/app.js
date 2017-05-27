925483855db8426ddc66867d7aca73f1df9e3ba0
Edward Betts <edward@4angle.com>
2017-05-27 14:34:54 +0100

    add 'show all tags' link next to candidates
    closes: #47

---
+++ matcher/static/js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $('.all_tags').hide();
+
+  $('.show_tags_link').click(function(e) {
+    var link = $(this);
+    var show_all = 'show all tags';
+    $('#candidate' + link.data('key')).toggle();
+    link.text(link.text() == show_all ? 'hide tags' : show_all);
+
+  });
+});
