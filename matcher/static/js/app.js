59c0f964dc26400e565d82620a71a5cb0723f45a
Edward Betts <edward@4angle.com>
2017-06-06 19:38:51 +0000

    save bad match without loading new pagecloses: #84

--- matcher/static/js/app.js
+++ matcher/static/js/app.js
@@ -1,7 +1,10 @@
+'use strict';
+
 $(function() {
 
   $('.all-tags').hide();
   $('.bad-match').hide();
+  $('.bad-reported').hide();
   $('#oql').hide();
 
 
@@ -31,6 +34,22 @@ $(function() {
     e.preventDefault();
     $('#oql').toggle();
     link.text(link.text() == show ? 'hide query' : show);
+  });
 
+  $('.bad-match-save').click(function(e) {
+    var link = $(this);
+    e.preventDefault();
+    var comment = $('#comment-' + link.data('key')).val();
+    var item_id = link.data('item');
+    var osm_id = link.data('osm-id');
+    var osm_type = link.data('osm-type');
+
+    var url = '/bad_match/Q' + item_id + '/' + osm_type + '/' + osm_id;
+
+    $.post(url, {'comment': comment}).done(function(data) {
+        $('#bad-match-' + link.data('key')).hide();
+        $('#reported-' + link.data('key')).show();
+        $('#report-link-' + link.data('key')).hide();
+    });
   });
 });
