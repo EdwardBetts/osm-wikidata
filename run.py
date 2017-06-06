cb495e25d131fe492464eca3c2212641b6a9a9fc
Edward Betts <edward@4angle.com>
2017-06-06 09:32:12 +0100

    add debugtoolbar

--- run.py
+++ run.py
@@ -3,10 +3,19 @@
 from matcher.view import app
 from matcher import database
 from matcher.error_mail import setup_error_mail
+from flask_debugtoolbar import DebugToolbarExtension
+# from werkzeug.debug import DebuggedApplication
+# from werkzeug.serving import run_simple
 
 if __name__ == "__main__":
     app.config.from_object('config.default')
     database.init_app(app)
     setup_error_mail(app)
     app.debug = False
-    app.run('0.0.0.0', port=5001)
+    app.debug = True
+    toolbar = DebugToolbarExtension(app)
+    # debug_app = DebuggedApplication(app, evalex=False)
+
+    app.run(port=5001)
+
+    # run_simple('localhost', 5001, debug_app, use_reloader=True)
