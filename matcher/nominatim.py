647e6c3e22308a90ebe6680d0f56628c61de517e
Edward Betts <edward@4angle.com>
2017-09-25 20:19:30 +0000

    add get_us_county and get_us_city functions

--- matcher/nominatim.py
+++ matcher/nominatim.py
@@ -31,3 +31,34 @@ def lookup_with_params(**kwargs):
 
 def lookup(q):
     return lookup_with_params(q=q)
+
+def get_us_county(name, state):
+    if ' ' not in name and 'county' not in name:
+        name += ' county'
+    results = lookup(q='{}, {}'.format(name, state))
+
+    def pred(hit):
+        return (hit['osm_type'] != 'node' and
+                'county' in hit['display_name'].lower())
+    return next(filter(pred, results), None)
+
+def get_us_city(name, state):
+    results = lookup_with_params(city=name, state=state)
+    if len(results) != 1:
+        results = [hit for hit in results
+                   if hit['type'] == 'city' or hit['osm_type'] == 'node']
+        if len(results) != 1:
+            print('more than one')
+            return
+    hit = results[0]
+    if hit['type'] not in ('administrative', 'city'):
+        print('not a city')
+        return
+    if hit['osm_type'] == 'node':
+        print('node')
+        return
+    if not hit['display_name'].startswith(name):
+        print('wrong name')
+        return
+    assert ('osm_type' in hit and 'osm_id' in hit and 'geotext' in hit)
+    return hit
