becf9e920e7fd51f9c692c6b4bf2f03b4ef6334d
Edward Betts <edward@4angle.com>
2018-11-07 14:29:59 +0000

    Special case for Antarctica.

--- matcher/place.py
+++ matcher/place.py
@@ -195,7 +195,8 @@ class Place(Base):
     @property
     def too_big(self):
         max_area = current_app.config['PLACE_MAX_AREA']
-        return self.area_in_sq_km > max_area
+        antarctica = 2186646
+        return int(self.osm_id) != antarctica and self.area_in_sq_km > max_area
 
     def update_from_nominatim(self, hit):
         if self.place_id != int(hit['place_id']):
