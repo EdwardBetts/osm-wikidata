933335afa6ea7e5e3532b8253693fb1b626c4ce8
Edward Betts <edward@4angle.com>
2019-05-24 13:24:43 +0100

    Make thumbnail optional.

--- matcher/commons.py
+++ matcher/commons.py
@@ -13,10 +13,15 @@ def image_detail(commons_filename, thumbwidth=None):
     r = requests.get(commons_api_url, params=params)
     root = lxml.etree.fromstring(r.content)
     file_element = root.find('./file')
+    thumb_element = file_element.find('./urls/thumbnail')
 
-    return {
+    ret = {
         'image': file_element.find('./urls/file').text,
         'height': file_element.find('./height').text,
         'width': file_element.find('./width').text,
-        'thumbnail': file_element.find('./urls/thumbnail').text,
     }
+
+    if thumb_element is not None:
+        ret['thumbnail'] = thumb_element.text
+
+    return ret
