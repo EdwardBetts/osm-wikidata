d3188a88ce46664de336ceeb3ce9b4cab6709ebb
Edward Betts <edward@4angle.com>
2019-12-12 08:08:40 +0000

    Bug fix.

--- matcher/wikidata.py
+++ matcher/wikidata.py
@@ -696,7 +696,10 @@ def page_banner_from_entity(entity, thumbwidth=None):
 
     filename = entity['claims'][property_key][0]['mainsnak']['datavalue']['value']
 
-    return commons.image_detail([filename], thumbwidth=thumbwidth)[0]
+    try:
+        return commons.image_detail([filename], thumbwidth=thumbwidth)[0]
+    except IndexError:
+        return
 
 def entity_label(entity, language=None):
     if language and language in entity['labels']:
