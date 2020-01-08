b470cd0fb594ef9d3d3b99f93183932894f928de
Edward Betts <edward@4angle.com>
2020-01-08 10:30:50 +0000

    Add more detail to error mails.

--- matcher/error_mail.py
+++ matcher/error_mail.py
@@ -1,29 +1,42 @@
 import logging
 from logging.handlers import SMTPHandler
 from logging import Formatter
+from flask import request
+
+PROJECT = 'osm-wikidata'
 
 class MatcherSMTPHandler(SMTPHandler):
     def getSubject(self, record):  # noqa: N802
-        return ('osm-wikidata error: {}'.format(record.exc_info[0].__name__)
+        return (f'{PROJECT} error: {record.exc_info[0].__name__}'
                 if (record.exc_info and record.exc_info[0])
-                else 'osm-wikidata error: {}:{:d}'.format(record.pathname, record.lineno))
+                else f'{PROJECT} error: {record.pathname}:{record.lineno:d}')
+
+class RequestFormatter(Formatter):
+    def format(self, record):
+        record.request = request
+        return super().format(record)
+
 
 def setup_error_mail(app):
+    formatter = RequestFormatter('''
+    Message type:       {levelname}
+    Location:           {pathname:s}:{lineno:d}
+    Module:             {module:s}
+    Function:           {funcName:s}
+    Time:               {asctime:s}
+    GET args:           {request.args!r}
+    URL:                {request.url}
+
+    Message:
+
+    {message:s}
+    ''', style='{')
+
     mail_handler = MatcherSMTPHandler(app.config['SMTP_HOST'],
                                       app.config['MAIL_FROM'],
                                       app.config['ADMINS'],
                                       app.name + ' error')
-    mail_handler.setFormatter(Formatter('''
-    Message type:       %(levelname)s
-    Location:           %(pathname)s:%(lineno)d
-    Module:             %(module)s
-    Function:           %(funcName)s
-    Time:               %(asctime)s
-
-    Message:
-
-    %(message)s
-    '''))
+    mail_handler.setFormatter(formatter)
 
     mail_handler.setLevel(logging.ERROR)
     app.logger.propagate = True
