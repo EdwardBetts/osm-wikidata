efd6baffb5548807b82cfb4d2eded999dea2dbe8
Edward Betts <edward@4angle.com>
2020-06-12 22:49:33 +0100

    Bug fix names from part of

--- matcher/model.py
+++ matcher/model.py
@@ -458,15 +458,16 @@ https://www.wikidata.org/wiki/{self.qid}
 
     def names(self):
         part_of_names = set()
-        for p361 in self.entity['claims'].get('P361', []):
-            try:
-                part_of_id = p361['mainsnak']['datavalue']['value']['numeric-id']
-            except KeyError:
-                continue
-            # TODO: download item if it doesn't exist
-            part_of_item = Item.query.get(part_of_id)
-            if part_of_item:
-                part_of_names |= part_of_item.names().keys()
+        if self.entity and 'claims' in self.entity:
+            for p361 in self.entity['claims'].get('P361', []):
+                try:
+                    part_of_id = p361['mainsnak']['datavalue']['value']['numeric-id']
+                except KeyError:
+                    continue
+                # TODO: download item if it doesn't exist
+                part_of_item = Item.query.get(part_of_id)
+                if part_of_item:
+                    part_of_names |= part_of_item.names().keys()
 
         d = wikidata.names_from_entity(self.entity) or defaultdict(list)
         for name in self.extract_names or []:
