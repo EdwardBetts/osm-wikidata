735f36c9747c99b2d3cdb1381b1ee276645dfbe1
Edward Betts <edward@4angle.com>
2020-11-14 14:40:51 +0000

    Add Rijksmonument ID

--- matcher/model.py
+++ matcher/model.py
@@ -417,6 +417,7 @@ https://www.wikidata.org/wiki/{self.qid}
             # ('P281', ['addr:postcode', 'postal_code'], 'postal code'),
             ('P296', ['ref', 'ref:train', 'railway:ref'], 'station code'),
             ('P300', ['ISO3166-2'], 'ISO 3166-2 code'),
+            ('P359', ['ref:rce'], 'Rijksmonument ID'),
             ('P590', ['ref:gnis', 'GNISID', 'gnis:id', 'gnis:feature_id'], 'USGS GNIS ID'),
             ('P649', ['ref:nrhp'], 'NRHP reference number'),
             ('P722', ['uic_ref'], 'UIC station code'),
