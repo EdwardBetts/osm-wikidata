dfc85e928f672418d914f4d8cd6abf4915bea24c
Edward Betts <edward@4angle.com>
2022-10-12 16:27:52 +0100

    Add docstrings and types

--- matcher/chat.py
+++ matcher/chat.py
@@ -1,20 +1,23 @@
+"""Protocol for communication over a socket."""
+
 import json
 import socket
+from typing import Any
 
 
-def connect_to_queue():
+def connect_to_queue() -> socket.socket:
+    """Connect to the matcher queue."""
     address = ("localhost", 6030)
     sock = socket.create_connection(address)
     sock.setblocking(True)
     return sock
 
 
-def read_line(sock):
+def read_line(sock: socket.socket) -> str | None:
     """Read a single line from a socket. The line is terminated with CR+LF."""
-
     char = sock.recv(1)
     if char == b"":
-        return
+        return None
     buf = b""
     while char != b"\r":
         buf += char
@@ -24,15 +27,18 @@ def read_line(sock):
     return buf.decode("utf-8")
 
 
-def send_msg(sock, msg):
+def send_msg(sock: socket.socket, msg: str) -> None:
+    """Send a message over a socket."""
     return sock.sendall(msg.encode("utf-8") + b"\r\n")
 
 
-def send_json(sock, msg):
+def send_json(sock: socket.socket, msg: Any) -> None:
+    """Send JSON over a socket."""
     return send_msg(sock, json.dumps(msg))
 
 
-def send_command(sock, cmd, **params):
+def send_command(sock: socket.socket, cmd: str, **params: dict[Any, Any]) -> None:
+    """Send a command over a socket."""
     if params:
         msg = cmd + " " + json.dumps(params)
     else:
@@ -40,7 +46,7 @@ def send_command(sock, cmd, **params):
     return send_msg(sock, msg)
 
 
-def read_json_line(sock):
+def read_json_line(sock: socket.socket) -> Any | None:
+    """Read a line of JSON from a socket."""
     line = read_line(sock)
-    if line:
-        return json.loads(line)
+    return json.loads(line) if line else None
