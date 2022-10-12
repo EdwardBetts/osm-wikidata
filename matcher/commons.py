6134957b1c7195031e2e183a0a385184bed267b1
Edward Betts <edward@4angle.com>
2022-10-12 17:21:08 +0100

    Add types and docstrings.

--- matcher/commons.py
+++ matcher/commons.py
@@ -1,18 +1,27 @@
-import requests
+"""Wikimedia Commons API call."""
+
 import urllib.parse
+from typing import Any
+
+import requests
+
 from . import utils
 
 commons_start = "http://commons.wikimedia.org/wiki/Special:FilePath/"
 commons_url = "https://www.wikidata.org/w/api.php"
 page_size = 50
 
+CallParams = dict[str, str | int]
 
-def commons_uri_to_filename(uri):
+
+def commons_uri_to_filename(uri: str) -> str:
+    """Convert commons URI to a filename."""
     return urllib.parse.unquote(utils.drop_start(uri, commons_start))
 
 
-def api_call(params):
-    call_params = {
+def api_call(params: CallParams) -> requests.Response:
+    """Call the Commons API."""
+    call_params: CallParams = {
         "format": "json",
         "formatversion": 2,
         **params,
@@ -21,8 +30,9 @@ def api_call(params):
     return requests.get(commons_url, params=call_params, timeout=5)
 
 
-def image_detail(filenames, thumbheight=None, thumbwidth=None):
-    params = {
+def image_detail_params(thumbheight: int | None, thumbwidth: int | None) -> CallParams:
+    """Image detail params."""
+    params: CallParams = {
         "action": "query",
         "prop": "imageinfo",
         "iiprop": "url",
@@ -32,14 +42,26 @@ def image_detail(filenames, thumbheight=None, thumbwidth=None):
     if thumbwidth is not None:
         params["iiurlwidth"] = thumbwidth
 
+    return params
+
+
+def api_image_detail_call(params: CallParams, cur: list[str]) -> requests.Response:
+    """Image details API call."""
+    call_params = params.copy()
+    call_params["titles"] = "|".join(f"File:{f}" for f in cur)
+
+    return api_call(call_params)
+
+
+def image_detail(
+    filenames: list[str], thumbheight: int | None = None, thumbwidth: int | None = None
+) -> dict[str, Any]:
+    """Get image detail from Wikimedia Commons."""
+    params = image_detail_params(thumbheight, thumbwidth)
+
     images = {}
-
     for cur in utils.chunk(filenames, page_size):
-        call_params = params.copy()
-        call_params["titles"] = "|".join(f"File:{f}" for f in cur)
-
-        r = api_call(call_params)
-
+        r = api_image_detail_call(params, cur)
         for image in r.json()["query"]["pages"]:
             filename = utils.drop_start(image["title"], "File:")
             images[filename] = image["imageinfo"][0] if "imageinfo" in image else None
