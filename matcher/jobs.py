7ed6ce52f6835c3fde32beb7d031727a28dedc89
Edward Betts <edward@4angle.com>
2024-04-06 16:31:55 +0200

    Add type hints and docstrings

--- matcher/jobs.py
+++ matcher/jobs.py
@@ -1,18 +1,24 @@
 """Jobs."""
 
+import typing
 from datetime import datetime
 
 from . import chat
 from .place import Place
 
+StrDict = dict[str, typing.Any]
 
-def matcher_queue_request(command, **params):
+
+def matcher_queue_request(
+    command: str, **params: dict[typing.Any, typing.Any]
+) -> StrDict:
+    """Make request to matcher queue."""
     sock = chat.connect_to_queue()
     chat.send_command(sock, command, **params)
 
     replies = []
     while True:
-        msg = chat.read_json_line(sock)
+        msg: StrDict | None = chat.read_json_line(sock)
         if msg is None:
             break
         replies.append(msg)
@@ -23,22 +29,26 @@ def matcher_queue_request(command, **params):
     return replies[0]
 
 
-def parse_job_start(start):
+def parse_job_start(start: str) -> datetime:
+    """Parse timestamp string to datetime."""
     return datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
 
 
-def get_job(place):
+def get_job(place: Place) -> StrDict | None:
+    """Get job for given place."""
     reply = matcher_queue_request("jobs")
     assert reply["type"] == "jobs"
 
+    job: StrDict
     for job in reply["items"]:
         if not all(job[key] == getattr(place, key) for key in ("osm_type", "osm_id")):
             continue
         job["start"] = parse_job_start(job["start"])
         return job
+    return None
 
 
-def get_jobs():
+def get_jobs() -> list[StrDict]:
     """Get jobs from matcher queue."""
     reply = matcher_queue_request("jobs")
     assert reply["type"] == "jobs"
